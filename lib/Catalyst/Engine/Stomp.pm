d87ab9ceb662af38d3bb04653809df75a7c1637b
Chris Andrews <chris@nodnol.org>
2009-08-27 22:42:56 +0100

    Version bump.

--- lib/Catalyst/Engine/Stomp.pm
+++ lib/Catalyst/Engine/Stomp.pm
@@ -8,7 +8,7 @@ use namespace::autoclean;
 
 extends 'Catalyst::Engine::Embeddable';
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 has connection => (is => 'rw', isa => 'Net::Stomp');
 has conn_desc => (is => 'rw', isa => Str);
